a2e4ce45ef793fcdd7f747f7042e678de27e226d
Elizabeth Engelman <elizabeth@redsquirrel.com>
2022-11-01 22:53:33 -0400

    Create a basic drawing app

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 //Initialize the express 'app' object
 let express = require("express");
 let app = express();
+let io = require("socket.io");
 
 app.use("/", express.static("public"));
 
 //Initialize the actual HTTP server
 let http = require("http");
+const { emit } = require("process");
 let server = http.createServer(app);
 
-//'port' variable allowd for deployment
+//'port' variable allows for deployment
 let port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log("Server listening at port: " + port);
 });
 
 //Socket.io Code
+io = new io.Server(server);
+io.on("connection", (socket) => {
+  console.log("a user is connected", socket.id);
+
+  //by doing it in here, we can track which socket is message
+  socket.on("mouseMoved", (data) => {
+    //send message to all clients, including the sender
+    io.emit("mouseMovedAll", data);
+    //socket.broadcast or something tells everyone but the sender
+    // socket.emit to send it back to the original sender
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A client has disconnected: " + socket.id);
+  });
+});
--- public/app.js
+++ public/app.js
@@ -1,1 +1,37 @@
 console.log("Hello!");
+
+let socket = io();
+
+socket.on("connection", (socket) => {
+  console.log("socket conncted");
+});
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
+  socket.on("mouseMovedAll", (data) => {
+    drawPosition(data);
+  });
+}
+
+
+function mouseMoved() {
+  fill(0);
+  ellipse(mouseX, mouseY, 10, 10);
+
+  let mousePos = { x: mouseX, y: mouseY };
+  socket.emit("mouseMoved", mousePos);
+}
+
+addEventListener("click", (event) => {});
+
+onclick = (event) => {
+  woo(mouseX, mouseY);
+  socket.emit("mouseClicked", mouseX, mouseY);
+};
+
+function drawPosition(positionObj) {
+  fill(0);
+  ellipse(positionObj.x, positionObj.y, 10, 10);
+}
